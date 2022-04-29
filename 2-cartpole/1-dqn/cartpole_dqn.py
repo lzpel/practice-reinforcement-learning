cb22af57e020ff5336d3af6a32b679869cdfdcde
lzpel <18492524+lzpel@users.noreply.github.com>
2022-04-29 12:21:10 +0900

    2-1

--- 2-cartpole/1-dqn/cartpole_dqn.py
+++ 2-cartpole/1-dqn/cartpole_dqn.py
@@ -4,9 +4,9 @@ import pylab
 import random
 import numpy as np
 from collections import deque
-from keras.layers import Dense
-from keras.optimizers import Adam
-from keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Sequential
 
 EPISODES = 300
 
